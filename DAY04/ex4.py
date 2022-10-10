3302b468afff85150d81b6977adb281a14e0cff7
mani015 <ramanadhulamanikumar01975@gmail.com>
2022-10-10 12:07:55 +0530

    python

--- DAY04/ex4.py
+++ DAY04/ex4.py
@@ -1,8 +1,10 @@
-l=[11,22,33,44,55,66,77,12.0,52.30,'python','java',44]
-print(l[-1])
-print(l[-3])
-print(l[-5])
-print(l[-2])
-print(l[-4])
-print(l[-10])
-print(l[-12])
+# l=[11,22,33,44,55,66,77,12.0,52.30,'python','java',44]
+# print(l[-1])
+# print(l[-3])
+# print(l[-5])
+# print(l[-2])
+# print(l[-4])
+# print(l[-10])
+# print(l[-12])
+
+
---
+++ DAY05/ex1.py
@@ -0,0 +1,8 @@
+# Append : this method is adding data ,end of the list
+l=[1,2,3,4,5,6,'apple','melon','banana']
+# print(l)
+# l.append('python')
+# print(l)
+l.append('java','jango')
+print(l)
+# TypeError: list.append() takes exactly one argument (2 given)
