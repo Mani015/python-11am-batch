f679b5e3eb6f763a089bfe84912c61649c947a43
mani015 <ramanadhulamanikumar01975@gmail.com>
2022-10-24 18:51:22 +0530

    'python'

---
+++ DAY14/ex1.py
@@ -0,0 +1,7 @@
+number=int(input('Enter one number'))
+
+if number%2==0:
+    print('This number is even')
+
+else:
+    print('This number is odd')
---
+++ DAY14/ex3.py
@@ -0,0 +1,10 @@
+num=int(input('Enter one number:'))
+
+if num==0:
+    print('THis is even ')
+elif num==20:
+    print('20 is correct number')
+elif num==10:
+    print('10 is correct number')
+else:
+    print('this is wrong number')
